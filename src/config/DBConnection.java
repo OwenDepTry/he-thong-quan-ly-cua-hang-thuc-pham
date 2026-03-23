49947b012ed474d13a3e1f283d1e0eaea75841aa
Phan Tien Dat <tiendatsgu298@gmail.com>
2026-03-23 14:45:57 +0700

    fix: DBConnection syntax error

--- src/config/DBConnection.java
+++ src/config/DBConnection.java
@@ -5,10 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/qlcuahangthucpham?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Ho_Chi_Minh";
     private static final String USER = "root";
     private static final String PASSWORD = "ngaoda110";
-return new LoginResult(LoginStatus.DB_ERROR, null, e.toString());
+
     public static Connection open() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -17,10 +18,10 @@ return new LoginResult(LoginStatus.DB_ERROR, null, e.toString());
             return conn;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            throw new RuntimeException("Thiếu MySQL JDBC Driver: " + e.getMessage(), e);
+            throw new RuntimeException("Thiếu driver MySQL", e);
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new RuntimeException("Lỗi SQL khi kết nối: " + e.getMessage(), e);
+            throw new RuntimeException("Lỗi kết nối DB", e);
         }
     }
 }
